bc8427d4da5c3ecb718006315004517eff2ec158
Johnny <theLephty@gmail.com>
2015-10-23 10:20:08 -0600

    populate rolls with clicks

--- script.js
+++ script.js
@@ -7,10 +7,15 @@ app.controller('bowlController', ['$scope', function ($scope) {
     $scope.score = 0;
 
     $scope.sub = 0;
+    $scope.aScore = [];
 
-    $scope.scoreAdd = function () {
-        $scope.score += $scope.sub;
-        console.log("Pressed a button.");
+    $scope.rollAdd = function () {
+        if ($scope.aScore.length < 21) {
+
+            $scope.aScore.push($scope.sub);
+            console.log("Pressed a button.");
+        }
+
     };
 
 }]);
