10552da41f4034e55d930d97b55694cc5b9d7381
MATC <MATC>
2015-11-03 10:21:50 -0700

    angular hacks!
    not best practices :-P

--- script.js
+++ script.js
@@ -4,29 +4,75 @@ var app = angular.module('bowlApp', []);
 
 app.controller('bowlController', ['$scope', function ($scope) {
 
-    var frames = 10,
-        pins = 10,
-        rolls = 21;
+    var nFrames = 10,
+        nPins = 10,
+        nRolls = 21;
 
     $scope.superBowl = {
-        numRoll: 0,
-        numUserSubmission: 0,
-        arrScore: [],
-        arrFrames: [],
-        rollsPerFrame: 2,
-        rollsLastFrame: 3,
+        nRoll: 0,
+        nUserInput: 0,
+        aScore: [],
+        aFrames: [],
+        aPinInfo: [{
+            nVal: 0,
+            sName: "Zero"
+        }, {
+            nVal: 1,
+            sName: "One"
+        }, {
+            nVal: 2,
+            sName: "Two"
+        }, {
+            nVal: 3,
+            sName: "Three"
+        }, {
+            nVal: 4,
+            sName: "Four"
+        }, {
+            nVal: 5,
+            sName: "Five"
+        }, {
+            nVal: 6,
+            sName: "Six"
+        }, {
+            nVal: 7,
+            sName: "Seven"
+        }, {
+            nVal: 8,
+            sName: "Eight"
+        }, {
+            nVal: 9,
+            sName: "Nine"
+        }, {
+            nVal: 10,
+            sName: "Ten"
+        }],
+        nRollsPerFrame: 2,
+        nRollsLastFrame: 3,
 
-        rollAdd: function () {
-            if (!this.arrScore[this.numRoll] && this.numRoll < 21) {
-                this.arrScore[this.numRoll] = this.numUserSubmission;
+        rollAdd: function (val) {
+            if (!this.aScore[this.nRoll] && this.nRoll < 21) {
+                this.aScore[this.nRoll] = val;
+                this.nUserInput = val;
                 console.log("Pressed a button.");
             }
-            this.numRoll++;
+            this.nRoll++;
+        },
+
+        isDisabled: function (button) {
+            if (this.nRoll % 2 && (button.nVal + this.nUserInput) > 10) {
+                return true;
+            } else {
+                
+                return false;
+            }
+
+
         }
     };
 
-    $scope.superBowl.arrFrames.length = frames;
-    $scope.superBowl.arrScore.length = rolls;
+    $scope.superBowl.aFrames.length = nFrames;
+    $scope.superBowl.aScore.length = nRolls;
 
 
 }]);
